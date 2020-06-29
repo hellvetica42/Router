11d044d2be8a2c72a29b0f5a55ee4362f2a6dbd5
hellvetica42 <petar.slavicek@gmail.com>
2020-06-29 22:27:47 +0200

    Added absolute path

--- router.sh
+++ router.sh
@@ -3,10 +3,14 @@
 KEY='100% packet loss'
 PING="$(ping -c 1 -W 2 google.com)"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+LOG="${DIR}/login.sh"
+SMS="${DIR}/sendSMS.sh"
+
 if [[ $PING =~ "$KEY" ]] 
 then
-	bash login.sh
-	bash sendSMS.sh 13880 NASTAVI
+	bash $LOG
+	bash $SMS 13880 NASTAVI
 	echo "SMS sent on $(date)"
 	sleep 20
 else
--- sendSMS.sh
+++ sendSMS.sh
@@ -9,9 +9,13 @@ for (( i=0; i<${#HEX}; i+=2 )); do
 done
 
 CMD="curl -d \"isTest=false&goformId=SEND_SMS&notCallback=true&Number=${NUMBER}&sms_time=20%3B05%3B01%3B06%3B59%3B58%3B-4&MessageBody=${MSG}&ID=-1&encode_type=GSM7_default\" http://192.168.0.1/goform/goform_set_cmd_process"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+DIR="${DIR}/headers.txt"
+
 while read LINE
 	do CMD="${CMD} -H \"$LINE\" "
-done < headers.txt
+done < $DIR
 
 #echo $CMD
 eval $CMD
